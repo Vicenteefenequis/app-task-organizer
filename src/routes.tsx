cd5ab2ae064fc33b9785ac3ae1f296e45c351b90
Vicente Junior <vicente19981@live.com>
2023-04-16 16:00:08 -0300

    refactor: rename lazy task

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,15 +1,15 @@
 import React, { Suspense, lazy } from 'react';
 import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
 
-const LazyApp = lazy(() => import('#/containers/App'));
-const LazyNotFound = lazy(() => import('#/containers/NotFound'));
+const LazyTask = lazy(() => import('./containers/Task'));
+const LazyNotFound = lazy(() => import('./containers/NotFound'));
 
 const Routes: React.FC = () => {
   return (
     <Router>
       <Suspense>
         <Switch>
-          <Route exact path="/" component={LazyApp} />
+          <Route exact path="/" component={LazyTask} />
           <Route path="*" component={LazyNotFound} />
         </Switch>
       </Suspense>
