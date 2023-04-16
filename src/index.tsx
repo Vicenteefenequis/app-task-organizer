5d6782fe32e9eaaa8286e7456323bf05105a1cec
Vicente Junior <vicente19981@live.com>
2023-04-16 20:42:20 -0300

    feat: adding react-datepicker

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './styles/globals.scss';
 import Routes from './routes';
 import { ChakraProvider } from '@chakra-ui/react';
 import { theme } from './config/theme';
+import 'react-datepicker/dist/react-datepicker.css';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
