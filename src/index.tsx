661be188bac0ddd24268f38be36422545f400529
Vicente Junior <vicente19981@live.com>
2023-04-16 16:00:44 -0300

    feat: create routes

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './containers/App';
+import './styles/globals.scss';
+import Routes from './routes';
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Routes />
   </React.StrictMode>
 );
