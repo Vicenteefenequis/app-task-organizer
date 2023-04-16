ff0add6b88c5df304b5d09467b9128bd6e7e3aa1
Vicente Junior <vicente19981@live.com>
2023-04-16 16:27:40 -0300

    feat: adding on fallback of routers loader

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,5 +1,6 @@
 import React, { Suspense, lazy } from 'react';
 import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
+import Loader from './components/Loader';
 
 const LazyTask = lazy(() => import('./containers/Task'));
 const LazyNotFound = lazy(() => import('./containers/NotFound'));
@@ -7,7 +8,7 @@ const LazyNotFound = lazy(() => import('./containers/NotFound'));
 const Routes: React.FC = () => {
   return (
     <Router>
-      <Suspense>
+      <Suspense fallback={<Loader show />}>
         <Switch>
           <Route exact path="/" component={LazyTask} />
           <Route path="*" component={LazyNotFound} />
