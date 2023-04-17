b0a5fa7cfd012ef1df8d4fe3612d49bc01cf4d85
Vicente Junior <vicente19981@live.com>
2023-04-17 19:25:10 -0300

    feat: export modal form

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -2,3 +2,4 @@ export { default as Loader } from './Loader';
 export { default as Header } from './Header';
 export { default as AlertDialog } from './AlertDialog';
 export { default as DatePicker } from './DatePicker';
+export { default as ModalForm } from './ModalForm';
