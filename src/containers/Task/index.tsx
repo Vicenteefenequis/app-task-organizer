d2c03337ccd5a3cf27e07426d8fec4fd4d22c281
Vicente Junior <vicente19981@live.com>
2023-04-16 21:26:45 -0300

    feat: removing initialize

--- src/containers/Task/index.tsx
+++ src/containers/Task/index.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import { Header } from '#/components';
-import { Container, Input } from '@chakra-ui/react';
+import { Container } from '@chakra-ui/react';
 
 function Task() {
   return (
     <Container maxW="container.lg" bg="gray.400" mt={'24px'}>
       <Header name="Vicente Nascimento" tasks={2} />
-      <Input />
     </Container>
   );
 }
