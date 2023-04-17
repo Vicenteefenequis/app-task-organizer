ad3952e13996389b895c649b31f1db2fe5662856
Vicente Junior <vicente19981@live.com>
2023-04-16 22:31:19 -0300

    feat: create query client configuration

--- src/index.tsx
+++ src/index.tsx
@@ -4,14 +4,19 @@ import Routes from './routes';
 import { ChakraProvider } from '@chakra-ui/react';
 import { theme } from './config/theme';
 import 'react-datepicker/dist/react-datepicker.css';
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
+
+export const queryClient = new QueryClient();
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <ChakraProvider theme={theme}>
-      <Routes />
-    </ChakraProvider>
+    <QueryClientProvider client={queryClient}>
+      <ChakraProvider theme={theme}>
+        <Routes />
+      </ChakraProvider>
+    </QueryClientProvider>
   </React.StrictMode>
 );
