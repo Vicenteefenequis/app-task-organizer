c4633034bfb47e1ca51ebd5968c81ca391e3d88d
Vicente Junior <vicente19981@live.com>
2023-04-16 21:42:41 -0300

    feat: create add new task

--- src/containers/Task/index.tsx
+++ src/containers/Task/index.tsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import { Header } from '#/components';
-import { Container } from '@chakra-ui/react';
+import { Button, Container } from '@chakra-ui/react';
+import { FiPlusCircle } from 'react-icons/fi';
 
 function Task() {
   return (
     <Container maxW="container.lg" bg="gray.400" mt={'24px'}>
       <Header name="Vicente Nascimento" tasks={2} />
+      <Button
+        colorScheme="whiteAlpha"
+        leftIcon={<FiPlusCircle />}
+        variant="solid"
+        mt={'24px'}
+        color="black"
+      >
+        Adicionar nova tarefa
+      </Button>
     </Container>
   );
 }
